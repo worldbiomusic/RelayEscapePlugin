98843fed934d838b2b553c20adde783e8f59397e
worldbiomusic <worldbiomusic@gmail.com>
2021-02-04 19:48:38 +0900

    -이클립스 재설치

--- RelayEscapePlugin/src/com/wbm/plugin/Test.java
+++ RelayEscapePlugin/src/com/wbm/plugin/Test.java
@@ -1,28 +1,21 @@
 package com.wbm.plugin;
 
-import java.awt.Color;
-import java.awt.MouseInfo;
-import java.awt.Point;
-import java.awt.PointerInfo;
-import java.awt.Robot;
-import java.awt.event.InputEvent;
-
 public class Test {
     public static void main(String[] args) {
 	try {
 	    // 1510 25
 	    // 1510 125
 
-	    PointerInfo pt;
-
-	    while (true) {
-		Robot r = new Robot();
-		
-		    r.mousePress(InputEvent.BUTTON1_DOWN_MASK);
-		    r.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
-		
-		Thread.sleep(10);
-	    }
+//	    PointerInfo pt;
+//
+//	    while (true) {
+//		Robot r = new Robot();
+//		
+//		    r.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+//		    r.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+//		
+//		Thread.sleep(10);
+//	    }
 
 	} catch (Exception e) {
 	    // TODO Auto-generated catch block
